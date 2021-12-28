243f6c5e14619fa99e713c686845e3bb5cfa79d2
shaun <ycorredius@gmail.com>
2021-12-28 17:29:19 -0500

    cleaned forms

--- client/src/components/recipes/NewRecipeForm.js
+++ client/src/components/recipes/NewRecipeForm.js
@@ -1,17 +1,17 @@
 import React from "react";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import END_POINT from "../../actions/recipe/endpoint";
 import { useHistory } from "react-router-dom";
 
-function NewRecipeForm(props) {
+function NewRecipeForm() {
   const history = useHistory();
 
   const recipeName = React.useState("");
 
   const [indexes, setIndexes] = React.useState([]);
   const [counter, setCounter] = React.useState(0);
-  
+
   const { register, handleSubmit } = useForm();
 
   const [ingredientIndexes, setIngredientIndexes] = React.useState([]);
@@ -25,7 +25,7 @@ function NewRecipeForm(props) {
     setCounter((prevCounter) => prevCounter + 1);
   };
 
-  const addInstruction = () => {
+  const addInstruction = (props) => {
     setInstructionIndexes((prevInstructionIndexes) => [
       ...prevInstructionIndexes,
       instructionCounter,
@@ -43,8 +43,8 @@ function NewRecipeForm(props) {
 
   const onSubmit = (e) => {
     axios
-      .post(`${END_POINT}/recipes`,e, {
-        withCredentials: true
+      .post(`${END_POINT}/recipes`, e, {
+        withCredentials: true,
       })
       .then((response) => response.json)
       .then(history.push("/recipes"));
@@ -94,176 +94,183 @@ function NewRecipeForm(props) {
           onSubmit={handleSubmit(onSubmit)}
         >
           <fieldset name="recipe" key="recipe">
-          <div class="grid grid-cols-1  ">
-            <fieldset name={recipeName}>
-              <label class="block text-gray-700 font-bold mb-2 ">Name</label>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                ref={register}
-                name={`recipe[name]`}
-                placeholder="e.g. Spaghetti"
-              />
-            </fieldset>
-          </div>
-          <div>
-            {indexes.map((index) => {
-              const fieldName = `categories[${index}]`;
-              return (
-                <fieldset name={`recipe[${fieldName}]`} key={`recipe[${fieldName}]`}>
-                  <label class="block text-gray-700 font-bold mb-2 ">
-                    Tag
-                    <input
-                      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      type="text"
-                      name={`recipe[${fieldName}.tag]`}
-                      ref={register}
-                      required
-                    />
-                  </label>
-                  <div class='flex items-center justify-center'>
-                    <button
-                      type="button"
-                      onClick={removeCategory(index)}
-                      class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </fieldset>
-              );
-            })}
-            <div class="grid grid-cols-2 items-center justify-center">
-              <button
-                type="button"
-                onClick={addCategory}
-                class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                Add Category
-              </button>
-
-              <button
-                type="button"
-                onClick={clearCategories}
-                class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                Clear Categories
-              </button>
+            <div class="grid grid-cols-1  ">
+              <fieldset name={recipeName}>
+                <label class="block text-gray-700 font-bold mb-2 ">Name</label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  ref={register}
+                  name={`recipe[name]`}
+                  placeholder="e.g. Spaghetti"
+                />
+              </fieldset>
             </div>
-          </div>
-          <div>
-            {ingredientIndexes.map((index) => {
-              const fieldName = `ingredients[${index}]`;
-              return (
-                <fieldset name={`recipe[${fieldName}]`} key={`recipe[${fieldName}]`}>
-                  <div class="grid grid-cols-2 gap-2">
-                    <label class="block text-gray-700 font-bold mb-2 ">
-                      Item
-                      <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        name={`recipe[${fieldName}.name]`}
-                        ref={register}
-                        required
-                      />
-                    </label>
-
-                    <label class="block text-gray-700 font-bold mb-2 ">
-                      Quantiy
-                      <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        name={`recipe[${fieldName}.quantity]`}
-                        ref={register}
-                        required
-                      />
-                    </label>
-                  </div>
-                  <div class="flex items-center justify-center">
-                    <button
-                      type="button"
-                      onClick={removeIngredient(index)}
-                      class="bg-blue-dark hover:bg-gray-700 text-white font-bold px-4 py-2 m-2 rounded focus:outline-none focus:shadow-outline items-center justify-center"
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </fieldset>
-              );
-            })}
-            <div class="grid grid-cols-2 items-center justify-center">
-              <button
-                type="button"
-                onClick={addIngredient}
-                class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                Add Ingredient
-              </button>
-
-              <button
-                type="button"
-                onClick={clearIngredient}
-                class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                Clear Ingredients
-              </button>
-            </div>
-          </div>
-          <div>
-            {instructionIndexes.map((index) => {
-              const fieldName = `instructions[${index}]`;
-              return (
-                <fieldset name={`recipe[${fieldName}]`} key={`recipe[${fieldName}]`}>
-                  <label class="block text-gray-700 font-bold mb-2 ">
-                    Step 
-                    <input
-                      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      type="text"
-                      name={`recipe[${fieldName}.content]`}
-                      ref={register}
-                      required
-                    />
-                  </label>
-                <div class="flex items-center justify-center">
-
-                  <button
-                    type="button"
-                    onClick={removeInstruction(index)}
-                    class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+            <div>
+              {indexes.map((index) => {
+                const fieldName = `categories[${index}]`;
+                return (
+                  <fieldset
+                    name={`recipe[${fieldName}]`}
+                    key={`recipe[${fieldName}]`}
                   >
-                    
-                    Remove
-                  </button>
-                  </div>
-                </fieldset>
-              );
-            })}
-            <div class="grid grid-cols-2 items-center justify-center">
-              <button
-                type="button"
-                onClick={addInstruction}
-                class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              >
-                Add Instruction
-              </button>
+                    <label class="block text-gray-700 font-bold mb-2 ">
+                      Tag
+                      <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        name={`recipe[${fieldName}.tag]`}
+                        ref={register}
+                        required
+                      />
+                    </label>
+                    <div class="flex items-center justify-center">
+                      <button
+                        type="button"
+                        onClick={removeCategory(index)}
+                        class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </fieldset>
+                );
+              })}
+              <div class="grid grid-cols-2 items-center justify-center">
+                <button
+                  type="button"
+                  onClick={addCategory}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Add Category
+                </button>
 
+                <button
+                  type="button"
+                  onClick={clearCategories}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Clear Categories
+                </button>
+              </div>
+            </div>
+            <div>
+              {ingredientIndexes.map((index) => {
+                const fieldName = `ingredients[${index}]`;
+                return (
+                  <fieldset
+                    name={`recipe[${fieldName}]`}
+                    key={`recipe[${fieldName}]`}
+                  >
+                    <div class="grid grid-cols-2 gap-2">
+                      <label class="block text-gray-700 font-bold mb-2 ">
+                        Item
+                        <input
+                          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          type="text"
+                          name={`recipe[${fieldName}.name]`}
+                          ref={register}
+                          required
+                        />
+                      </label>
+
+                      <label class="block text-gray-700 font-bold mb-2 ">
+                        Quantiy
+                        <input
+                          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          type="text"
+                          name={`recipe[${fieldName}.quantity]`}
+                          ref={register}
+                          required
+                        />
+                      </label>
+                    </div>
+                    <div class="flex items-center justify-center">
+                      <button
+                        type="button"
+                        onClick={removeIngredient(index)}
+                        class="bg-blue-dark hover:bg-gray-700 text-white font-bold px-4 py-2 m-2 rounded focus:outline-none focus:shadow-outline items-center justify-center"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </fieldset>
+                );
+              })}
+              <div class="grid grid-cols-2 items-center justify-center">
+                <button
+                  type="button"
+                  onClick={addIngredient}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Add Ingredient
+                </button>
+
+                <button
+                  type="button"
+                  onClick={clearIngredient}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Clear Ingredients
+                </button>
+              </div>
+            </div>
+            <div>
+              {instructionIndexes.map((index) => {
+                const fieldName = `instructions[${index}]`;
+                return (
+                  <fieldset
+                    name={`recipe[${fieldName}]`}
+                    key={`recipe[${fieldName}]`}
+                  >
+                    <label class="block text-gray-700 font-bold mb-2 ">
+                      Step
+                      <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        name={`recipe[${fieldName}.content]`}
+                        ref={register}
+                        required
+                      />
+                    </label>
+                    <div class="flex items-center justify-center">
+                      <button
+                        type="button"
+                        onClick={removeInstruction(index)}
+                        class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </fieldset>
+                );
+              })}
+              <div class="grid grid-cols-2 items-center justify-center">
+                <button
+                  type="button"
+                  onClick={addInstruction}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Add Instruction
+                </button>
+
+                <button
+                  type="button"
+                  onClick={clearInstructions}
+                  class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Clear Instructions
+                </button>
+              </div>
+            </div>
+            <div class="flex items-center justify-center">
               <button
-                type="button"
-                onClick={clearInstructions}
                 class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
               >
-                Clear Instructions
+                Create
               </button>
             </div>
-          </div>
-          <div class="flex items-center justify-center">
-            <button
-              class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Create
-            </button>
-          </div>
           </fieldset>
         </form>
       </div>
--- client/src/components/recipes/UploadImage.js
+++ client/src/components/recipes/UploadImage.js
@@ -1,50 +1,51 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
-import axios from 'axios';
-import FormData from 'form-data';
+import axios from "axios";
+import FormData from "form-data";
 import END_POINT from "../../actions/recipe/endpoint";
 import { useHistory } from "react-router-dom";
 
 export default function UploadImage(props) {
-    const { register, handleSubmit } = useForm();
-    const history = useHistory();
+  const { register, handleSubmit } = useForm();
+  const history = useHistory();
 
   const onSubmit = (e) => {
-    let formData = new FormData()
+    let formData = new FormData();
 
-    formData.append('image', e.image[0])
-    formData.append('recipeId', e.recipeId)
-    axios.post(`${END_POINT}/recipes/${props.recipeId}/photos`, formData, {
-      withCredentials: true
-    })
-      .then(response => response.data)
-      .then(res => history.push(`/recipes/${res.data.id}`))
-  }
-    return (
-      <div class="container w-full max-w-xs">
-        <div cdlass="flex flex-col justify-center items-center">
-          <div class="flex font-bold text-blue-dark justify-center">
-            <h3>Upload image</h3>
-          </div>
-          <form
-            class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 "
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <input type="file" ref={register} name="image" />
-            <input
-              type="hidden"
-              value={props.recipeId}
-              ref={register}
-              name="recipeId"
-            />
-            <button
-              class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              submit
-            </button>
-          </form>
+    formData.append("image", e.image[0]);
+    formData.append("recipeId", e.recipeId);
+    axios
+      .post(`${END_POINT}/recipes/${props.recipeId}/photos`, formData, {
+        withCredentials: true,
+      })
+      .then((response) => response.data)
+      .then((res) => history.push(`/recipe/${res.data.id}`));
+  };
+  return (
+    <div class="container w-full max-w-xs">
+      <div cdlass="flex flex-col justify-center items-center">
+        <div class="flex font-bold text-blue-dark justify-center">
+          <h3>Upload image</h3>
         </div>
+        <form
+          class="bg-gray-300 shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input type="file" ref={register} name="image" />
+          <input
+            type="hidden"
+            value={props.recipeId}
+            ref={register}
+            name="recipeId"
+          />
+          <button
+            class="bg-blue-dark hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            submit
+          </button>
+        </form>
       </div>
-    );
+    </div>
+  );
 }
