8b2fbc56309f445aedb13f265485d79cfa824563
Shaun Yarbrough <shaun@plumcoownership.com>
2023-09-01 15:04:57 -0400

    updates cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:5173'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :delete, :options],
       credentials: true
   end
 end
