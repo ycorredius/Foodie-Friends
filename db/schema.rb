b3248144e84dd076eeca12ea76b91bb5aff87a51
Shaun Yarbrough <shaun@plumcoownership.com>
2023-09-01 21:15:07 -0400

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_02_004550) do
+ActiveRecord::Schema.define(version: 2023_09_02_010157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2023_09_02_004550) do
     t.string "tag"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_categories_on_recipe_id"
   end
 
   create_table "inboxes", force: :cascade do |t|
@@ -162,5 +164,6 @@ ActiveRecord::Schema.define(version: 2023_09_02_004550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "recipes"
   add_foreign_key "invitations", "users"
 end
