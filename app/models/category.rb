071087ae1e131015846d0cf79866c3326e85e29e
Shaun Yarbrough <shaun@plumcoownership.com>
2023-09-01 21:12:45 -0400

    adds recipe referernces to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,9 +6,18 @@
 #  tag        :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  recipe_id  :bigint
+#
+# Indexes
+#
+#  index_categories_on_recipe_id  (recipe_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recipe_id => recipes.id)
 #
 class Category < ApplicationRecord
-    #//TODO: Create a defined list of categories dont allow user to free text. 
-    #//TODO: Find some sort of list to read information that we can populate data with. 
-    
+  belongs_to :recipe, optional: true
+  # //TODO: Create a defined list of categories dont allow user to free text.
+  # //TODO: Find some sort of list to read information that we can populate data with.
 end
