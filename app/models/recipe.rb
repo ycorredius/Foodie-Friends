e5d0645f52909bda9ae0a441a02fbaf87a25f3e9
Shaun Yarbrough <shaun@plumcoownership.com>
2023-09-01 21:14:50 -0400

    removes unecessary associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,17 +13,13 @@
 #  user_id      :integer
 #
 
-#TODO: Build out a recipe search for category and ingredients separately.
-#TODO: Building recipe associations to able to be use without manual iteration through each param
+# TODO: Build out a recipe search for category and ingredients separately.
+# TODO: Building recipe associations to able to be use without manual iteration through each param
 class Recipe < ApplicationRecord
-    belongs_to :user
-    has_many :recipe_categories
-    has_many :categories, through: :recipe_categories
-    has_one_attached :image
-    
+  belongs_to :user
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
+  has_one_attached :image
 
-    accepts_nested_attributes_for :ingredients, allow_destroy: true
-    accepts_nested_attributes_for :categories, allow_destroy: true
-    accepts_nested_attributes_for :instructions, allow_destroy: true
-
+  accepts_nested_attributes_for :categories, allow_destroy: true
 end
