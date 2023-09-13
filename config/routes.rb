cb576f52f9a443bee3b0c6d06827dddd273346cc
Shaun Yarbrough <ycorredius@gmail.com>
2023-09-13 10:23:25 -0400

    adds devise back to project

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,10 @@ Rails.application.routes.draw do
   get "/logged_in", to: "sessions#is_logged_in?"
   # TODO: Create controller that handles the upload of images
   patch "/recipes/:id/upload_image", to: "recipes#upload_image"
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :auth
+    end
+  end
 end
