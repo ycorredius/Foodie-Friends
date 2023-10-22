3e7ce171de17cd2cddd3028089054642ef65b39b
Shaun Yarbrough <ycorredius@gmail.com>
2023-10-21 20:56:10 -0400

    updated recipe card on index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,15 @@
 class RecipesController < ApplicationController
-
+  before_action :set_recipe, only: %i[show update upload_image]
   def index
     @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
