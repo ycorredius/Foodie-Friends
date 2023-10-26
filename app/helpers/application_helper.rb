265ff6fe34f99eef0932d4b8ed986d08008f13b4
Shaun Yarbrough <ycorredius@gmail.com>
2023-10-26 13:16:21 -0400

    fixes user profile image and recipe image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def avatar_url(resource)
+    if resource.class == User
+      resource.avatar.blob
+    else
+      resource.image.attached? ? resource.image.blob : resource.avatar
+    end
+  end
 end
