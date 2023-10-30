bdfb93be228b018ac629c264806978763530aee9
Shaun Yarbrough <ycorredius@gmail.com>
2023-10-30 13:13:59 -0400

    updates nav bar to show active tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       resource.image.attached? ? resource.image.blob : resource.avatar
     end
   end
+
+  def active_tab(path)
+    current_page?(path) ? "border-indigo-500 text-indigo-600 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium" : "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+  end
 end
