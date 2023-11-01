4eb9d75570b98199b18fd6778343ef142d4ac1ee
Shaun Yarbrough <ycorredius@gmail.com>
2023-11-01 07:25:30 -0400

    updates recipe image and bug

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  def recipe_image(recipe)
+    unless recipe.image.nil?
+      recipe.image.attached? ? recipe.blob : recipe.avatar
+    else
+      "/default.jpg"
+    end
+  end
 end
