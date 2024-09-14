2842cb09d0df42e90e8c007a246d5a652cef208d
Shaun <33139203+ycorredius@users.noreply.github.com>
2024-09-14 09:12:53 -0400

    adds items to importmap pin (#24)

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,8 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-stimulus-components" # @5.1.1
+pin "stimulus-rails-nested-form" # @4.1.0
